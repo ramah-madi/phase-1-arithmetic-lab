530d8864968b521f9bf52b37e93a5c6a2a45a6a2
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-09-26 19:35:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 7;
+const num2 = 9;
+const num3 = 17;
+const num4 = 13;
+const multiply = (num1 * num2) - 1;
+const random = Math.ceil(Math.random() * 10);
+const mod = num3 % num4;
+const max = Math.max(2, 5, 10, 8, 20);
